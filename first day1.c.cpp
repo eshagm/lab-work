62e468ab4bb36489b0a30c7d3fbe2ce1a27fea4b
eshagm <131762914+eshagm@users.noreply.github.com>
2023-04-25 23:11:49 +0530

    Add files via upload

---
+++ first day1.c.cpp
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+main()
+{ 
+printf("---------\n");
+printf("|\t|\n");
+printf("e\t|\n");
+printf("s\t|\n");
+printf("h\t|\n");
+printf("a\t|\n");
+printf("|\t|\n");
+printf("---------\n");
+}
